b5746fc7c0dd60288459c2973b53ee94107e7e95
Guy J. Bianco IV <gjbianco@ncsu.edu>
2013-12-12 02:15:35 -0500

    initial commit

---
+++ 3and5/3and5.c
@@ -0,0 +1,70 @@
+/* +---------------------------------------------------------------------------+
+ * |                                                                           |
+ * | 3and5                                                                     |
+ * | Guy J. Bianco IV                                                          |
+ * |                                                                           |
+ * | Program that solves the first problem posted on Project Euler.            |
+ * |                                                                           |
+ * |   <http://projecteuler.net/problem=1>                                     |
+ * |                                                                           |
+ * |                                                                           |
+ * | From the page:                                                            |
+ * |                                                                           |
+ * |   "If we list all the natural numbers below 10 that are multiples of      |
+ * |    3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.        |
+ * |                                                                           |
+ * |    Find the sum of all the multiples of 3 or 5 below 1000."               |
+ * |                                                                           |
+ * |                                                                           |
+ * | History:                                                                  |
+ * |   Dec 12, 2013 - inital version                                           |
+ * |                                                                           |
+ * +---------------------------------------------------------------------------+
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* define our DEBUG macro as false if we are not in debug mode */
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
+/* the base numbers to obtain multiples from */
+#define MULT_A 3
+#define MULT_B 5
+
+/* the max number that the multiples cannot exceed */
+#define MAX 1000
+
+int main() {
+	int sum = 0;   /* running total */
+	int count = 0; /* iteration count (testing) */
+
+	/* calculate min number of iterations needed */
+	int iters = MAX/fmin(MULT_A, MULT_B);
+	
+	if(DEBUG)
+		printf("iters: %d\n", iters);
+
+	for(int i = 0; i < iters; i++) {
+		if(DEBUG)
+			count++;
+
+		/* TODO: optimize memory allocation */
+		int mult_a = MULT_A * i;
+		int mult_b = MULT_B * i;
+		
+		/* TODO: redundant? */
+		if(mult_a < MAX)
+			sum += mult_a;
+		if(mult_b < MAX)
+			sum += mult_b;
+	}
+
+	if(DEBUG)
+		printf("count: %d\n", count);
+
+	printf("sum: %d\n", sum);
+}
