11b8d7c06c4e95f31b3347c08c998dfd47a15a70
Guy J. Bianco IV <gjbianco@ncsu.edu>
2013-12-12 02:53:56 -0500

    optimizing

--- 3and5/3and5.c
+++ 3and5/3and5.c
@@ -42,8 +42,11 @@ int main() {
 	int sum = 0;   /* running total */
 	int count = 0; /* iteration count (testing) */
 
+	int mult_a = 0;
+	int mult_b = 0;
+
 	/* calculate min number of iterations needed */
-	int iters = MAX/fmin(MULT_A, MULT_B);
+	int iters = (MAX/fmin(MULT_A, MULT_B)) + 1;
 	
 	if(DEBUG)
 		printf("iters: %d\n", iters);
@@ -52,13 +55,11 @@ int main() {
 		if(DEBUG)
 			count++;
 
-		/* TODO: optimize memory allocation */
-		int mult_a = MULT_A * i;
-		int mult_b = MULT_B * i;
-		
-		/* TODO: redundant? */
+		mult_a += MULT_A;
 		if(mult_a < MAX)
 			sum += mult_a;
+
+		mult_b += MULT_B;
 		if(mult_b < MAX)
 			sum += mult_b;
 	}
